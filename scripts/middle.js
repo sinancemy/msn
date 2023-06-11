efd4791c217f5222f60462897b87f89912dc752f
bkulek20 <bkulek20@ku.edu.tr>
2023-06-11 21:23:55 +0300

    enjoyer profile updated

--- scripts/middle.js
+++ scripts/middle.js
@@ -102,9 +102,109 @@ async function getSavedPlaylists() {
     }
 }
 
+async function getFriends() {
+    const response = await fetch(`http://localhost:3001/getFriends`);
+    const data = await response.json();
+    var table = document.getElementById("enjoyer-friends-table");
+    for (let i = 0; i < data.length; i++) {
+        var row = table.insertRow();
+        // When row is clicked, go to the artist page with the proper id.
+        row.className = "clickable-table-row"
+        row.onclick = function () { showFriendsPanel(data[i].id); }
+        // Put image in cell
+        var coverCell = row.insertCell(0);
+        var img = document.createElement("img");
+        img.src = "data:image/png;base64," + btoa(String.fromCharCode.apply(null, data[i].cover_art.data));
+        img.className = "panel-image"
+        coverCell.appendChild(img);
+        // Put name in cell
+        var nameCell = row.insertCell(1);
+        nameCell.innerHTML = data[i].name;
+    }
+}
+async function getFriends() {
+    const response = await fetch(`http://localhost:3001/getFriends`);
+    const data = await response.json();
+    var table = document.getElementById("enjoyer-friends-table");
+    for (let i = 0; i < data.length; i++) {
+        var row = table.insertRow();
+        // When row is clicked, go to the artist page with the proper id.
+        row.className = "clickable-table-row"
+        row.onclick = function () { showFriendsPanel(data[i].id); }
+        // Put image in cell
+        var coverCell = row.insertCell(0);
+        var img = document.createElement("img");
+        img.src = "data:image/png;base64," + btoa(String.fromCharCode.apply(null, data[i].cover_art.data));
+        img.className = "panel-image"
+        coverCell.appendChild(img);
+        // Put name in cell
+        var nameCell = row.insertCell(1);
+        nameCell.innerHTML = data[i].name;
+    }
+}
 
 async function updateBio(userId, bio) {
     const response = await fetch(`http://localhost:3001/updateBio?userId=${userId}&bio=${bio}`);
+    const data = await response.json();
+    console.log("Result: ", data);
+}
+
+
+async function getCurrentUserInfo() {
+    const response = await fetch(`http://localhost:3001/getCurrentUserInfo`);
+    const data = await response.json();
+    console.log("Current User Info: ", data);
+}
+async function getUserInfo(userId) {
+    const response = await fetch(`http://localhost:3001/getUserInfo?userId=${userId}`);
+    const data = await response.json();
+    console.log("User Info: ", data);
+}
+
+
+
+async function getArtistInfo(artistId) {
+    const response = await fetch(`http://localhost:3001/getArtistInfo?artistId=${artistId}`);
+    const data = await response.json();
+    console.log("Artist Info: ", data);
+}
+
+async function getArtistTracks(artistId) {
+    const response = await fetch(`http://localhost:3001/getArtistTracks?artistId=${artistId}`);
+    const data = await response.json();
+    console.log("Artist Tracks: ", data);
+}
+
+async function getArtistFollowers(artistId) {
+    const response = await fetch(`http://localhost:3001/getArtistFollowers?artistId=${artistId}`);
+    const data = await response.json();
+    console.log("Artist Followers: ", data);
+}
+
+async function getArtistAppearedAlbums(artistId) {
+    const response = await fetch(`http://localhost:3001/getArtistAppearedAlbums?artistId=${artistId}`);
+    const data = await response.json();
+    console.log("Artist Appeared Albums: ", data);
+}
+
+async function addReaction(userId, contentId, text, emoji) {
+    const response = await fetch(`http://localhost:3001/addReaction?userId=${userId}&contentId=${contentId}&text=${text}&emoji=${emoji}`);
+    const data = await response.json();
+    console.log("Result: ", data);
+}
+
+// TODO: deleteReaction
+
+async function addEnjoyer(userId, enjoyment, username, password, fullName, avatar, email, bio) {
+    const response = await fetch(`http://localhost:3001/addEnjoyer?userId=${userId}&enjoyment=${enjoyment}&username=${username}&password=${password}&fullName=${fullName}&avatar=${avatar}&email=${email}&bio=${bio}`);
+    const data = await response.json();
+    console.log("Result: ", data);
+}
+
+// TODO: deleteEnjoyer
+
+async function addArtist(userId, verified, username, password, fullName, avatar, email, bio) {
+    const response = await fetch(`http://localhost:3001/addArtist?userId=${userId}&verified=${verified}&username=${username}&password=${password}&fullName=${fullName}&avatar=${avatar}&email=${email}&bio=${bio}`);
     const data = await response.json();
     console.log("Result: ", data);
 }
